3b309a944d49241e009c4c3940d097eaf2446328
gabrielzfx <chmodxxzrxx@gmail.com>
2020-10-15 02:55:26 +0300

    add new features in progress, add lists scan methods, add this module in testing module

--- modules/automap.rb
+++ modules/automap.rb
@@ -1,29 +1,13 @@
 =begin
 	
-HOST DISCOVERY:
-  -sL: List Scan - simply list targets to scan
-  -sP: Ping Scan - go no further than determining if host is online
-  -P0: Treat all hosts as online -- skip host discovery
-  -PS/PA/PU [portlist]: TCP SYN/ACK or UDP discovery probes to given ports
-  -PE/PP/PM: ICMP echo, timestamp, and netmask request discovery probes
-  -n/-R: Never do DNS resolution/Always resolve [default: sometimes resolve]
-  --dns-servers <serv1[,serv2],...>: Specify custom DNS servers
-  --system-dns: Use OS's DNS resolver
-SCAN TECHNIQUES:
-  -sS/sT/sA/sW/sM: TCP SYN/Connect()/ACK/Window/Maimon scans
-  -sN/sF/sX: TCP Null, FIN, and Xmas scans
-  --scanflags <flags>: Customize TCP scan flags
-  -sI <zombie host[:probeport]>: Idlescan
-  -sO: IP protocol scan
-  -b <ftp relay host>: FTP bounce scan
+
+
 PORT SPECIFICATION AND SCAN ORDER:
   -p <port ranges>: Only scan specified ports
     Ex: -p22; -p1-65535; -p U:53,111,137,T:21-25,80,139,8080
   -F: Fast - Scan only the ports listed in the nmap-services file)
   -r: Scan ports consecutively - don't randomize
 SERVICE/VERSION DETECTION:
-  -sV: Probe open ports to determine service/version info
-  --version-intensity <level>: Set from 0 (light) to 9 (try all probes)
   --version-light: Limit to most likely probes (intensity 2)
   --version-all: Try every single probe (intensity 9)
   --version-trace: Show detailed version scan activity (for debugging)
@@ -36,7 +20,6 @@ OS DETECTION:
 TIMING AND PERFORMANCE:
   Options which take <time> are in milliseconds, unless you append 's'
   (seconds), 'm' (minutes), or 'h' (hours) to the value (e.g. 30m).
-  -T[0-5]: Set timing template (higher is faster)
   --min-hostgroup/max-hostgroup <size>: Parallel host scan group sizes
   --min-parallelism/max-parallelism <numprobes>: Probe parallelization
   --min-rtt-timeout/max-rtt-timeout/initial-rtt-timeout <time>: Specifies
@@ -50,9 +33,8 @@ FIREWALL/IDS EVASION AND SPOOFING:
   -S <IP_Address>: Spoof source address
   -e <iface>: Use specified interface
   -g/--source-port <portnum>: Use given port number
-  --data-length <num>: Append random data to sent packets
   --ttl <val>: Set IP time-to-live field
-  --spoof-mac <mac address, prefix, or vendor name>: Spoof your MAC address
+ 
 	
 =end
 
@@ -69,16 +51,39 @@ FIREWALL/IDS EVASION AND SPOOFING:
 #------------------------------------------------------------
 
 module Automap
-	
-	port_scanner()
-		port = ''
-		target = ''
 
-		mac_vendor = []
-		scan_flags = []
+  $HOST_DISCOVERY = [
+		["-sL", "List Scan - simply list targets to scan"],
+  		["-sP", "Ping Scan - go no further than determining if host is online"],
+  		["-P0", "Treat all hosts as online -- skip host discovery"],
+  		["-PS", "[portlist]: TCP SYN discovery probes to given ports"],
+  		["-PA", "[portlist]: TCP ACK discovery probes to given ports"],
+  		["-PU", "[portlist]: TCP UDP discovery probes to given ports"],
+  		["-PE", "ICMP echo, timestamp, and netmask request discovery probes"],
+  		["-PP", "ICMP echo, timestamp, and netmask request discovery probes"],
+  		["-PM", "ICMP echo, timestamp, and netmask request discovery probes"],
+  		["-n", 	"Never do DNS resolution/Always resolve [default: sometimes resolve]"],
+  		["-R", 	"Never do DNS resolution/Always resolve [default: sometimes resolve]"],
+  		["--system-dns", "Use OS's DNS resolver"]
+  	]
 
-
-		# tranfor topics in functions
-
+  $SCAN_TECHNIQUES = [
+  		["-sS", "TCP SYN"],
+  		["-sT", "Connect()"],
+  		["-sA", "ACK"],
+  		["-sW", "Window"],
+  		["-sM", "Maimon scans"],
+  		["-sN", "TCP Null"],
+  		["-sF", "FIN"],
+  		["-sX", "Xmas scans"]
+  	]
+  		
+  $EVASION = [
+      ["--spoof-mac", "<mac address, prefix, or vendor name>: Spoof your MAC address"],
+      ["--data-length", "<num>: Append random data to sent packets"],
+      ["-T", "[0-5] Set timing template (higher is faster)"],
+      ["-sV", "Probe open ports to determine service/version info"],
+      ["--version-intensity", "<level>: Set from 0 (light) to 9 (try all probes)"]
+  ]
 
 end
