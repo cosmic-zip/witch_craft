b68ed3cc938f7a90c9cb757a50e2cbfab3e63d4a
gabrielzfx <chmodxxzrxx@gmail.com>
2020-10-24 16:52:38 -0300

    complete interpreter module, complete main function, move start.rb to interprete.rb

--- modules/interpreter.rb
+++ modules/interpreter.rb
@@ -8,44 +8,67 @@
 #
 #-------------------------------------------------------------
 
-module Start
+module Interpreter
 
-    def start(props)
-        
+    $PS1 = "[lookForMonkeys]: " 
+
+    def interpreter(props)    
         case props
-        when ""
-            Engine.INIT()
-        when ""
-            Engine.R()
-        when ""
-            Engine.cover()
-        when ""
-            Engine.compress()
-        when ""
-            Engine.port_scanner()
-        when ""
-            Engine.search()
-        when ""
-            Engine.status()
-        when ""
-            Engine.dns_scanner()
-        when ""
-            Visual.banner()
-        when ""
-            Visual.web_dns()
-        when ""
-            Visual.linux_files()
-        when ""
-            Visual.linux_folders()
-        when ""
-            Visual.linux_util()	
-        when ""
-            Automap.less_boring()
-		
-		
-		
-		
-		
-        
+            when 'exit'
+                exit
+            when 'update'
+                Engine.sys('git pull')
+            when 'train'
+                Engine.sys('sl')
+            when "INIT"
+                Engine.INIT()
+            when "reset"
+                Engine.R()
+            when "cover"
+                Engine.cover()
+            when "extract"
+                Engine.extract()
+            when "compress"
+                Engine.compress()
+            when "portscanner"
+                Engine.port_scanner()
+            when "search"
+                Engine.search()
+            when "status"
+                Engine.status()
+            when "dnsscanner"
+                Engine.dns_scanner()
+            when "dirscanner"
+                Engine.dir_scanner()
+            when "banner"
+                Visual.banner()
+            when "webdns"
+                Visual.web_dns()
+            when "linuxfiles"
+                Visual.linux_files()
+            when "linuxfolders"
+                Visual.linux_folders()
+            when "linuxutil"
+                Visual.linux_util()	
+            when "automap"
+                Automap.less_boring()
+            when "test"
+                Test.debug_all()
+            else
+                puts "#{$line}\n[COMMAND ERROR]: Option is ivalid, look for monkeys"
+        end
     end
+
+    def main()
+        # main while in russia
+        while true
+            interpreter('banner')
+            INTI()
+            while true
+                print($PS1); command = gets.chomp.to_s
+                interpreter(command)
+            end
+        end
+    end
+
 end
