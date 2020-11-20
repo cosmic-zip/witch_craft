36c19a6af2857d7831f489b0962e45206b98ae06
th3void <chmodxxzrxx@gmail.com>
2020-11-20 18:50:38 -0300

    add new feature: gem install dependencies

--- modules/install.rb
+++ modules/install.rb
@@ -8,15 +8,20 @@
 #-------------------------------------------------------------
 
 module Install
+
+	def ruby_dep
+		system 'gem install openssl builder'
+	end
 	
-	def fedora_linux()
+	def fedora_linux
 		prCyan "#{$line}Install dependences on Fedora Linux"
 		cmd = sys("dnf group install 'security lab' -y")
 		cmd == true ? prYellow("Dependences instaled") : prRed("[SYSTEM_ERROR]: dnf error")
  	end
 
- 	def arch_linux()
+ 	def arch_linux
  		# Nothing
- 	end
+    end
+
 
 end
