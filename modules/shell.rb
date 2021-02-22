2fa0f336664e6e7a58dde79b9e9da0e668745e5b
th3void <lnvoid@protonmail.com>
2021-02-22 11:59:36 -0500

    add fakedump to shell, fixed shell bugs

--- modules/shell.rb
+++ modules/shell.rb
@@ -14,59 +14,54 @@ module Shell
     def shell(opt)
 
         case opt
-                
-        when 'll'
-            sys('ls -lha')
-        when 'cls'
-            sys('clear')
-        when 'sht '
-            sys('shutdown -h now')
-        when 'rm '
-            sys('rm -rf')
-        when 'cp'
-            sys('cp -r')
-        when 'banner'
-            banner
-        when 'web_dns'
-            web_dns
-        when 'linux_files'
-            linux_files
-        when 'linux_folders'
-            linux_folders
-        when 'windows_files'
-            windows_files
-        when 'linux_util'
-            linux_util
-        when 'tor_search'
-            tor_search
-        when 'tor_alt'
-            tor_alt
-        when 'search'
-            search
-        when 'dns_scanner'
-            dns_scanner
-        when 'dir_scanner'
-            dir_scanner
-        when 'cover'
-            cover
-        when 'simple_map'
-            simple_map
-        when 'extract'
-            extract
-        when 'compress'
-            compress
-        when 'rg'
-            rg
-        when 'cpf'
-            cpf
-        when 'gem'
-            gem
-        when 'idhash'
-            idhash
+        when 'call_rg'
+            FakeDump.call_rg
+        when 'call_cpf'
+            FakeDump.call_cpf
+        when 'call_gem'
+            FakeDump.call_gem
         when 'simple_dump'
-            simple_dump
-        when 'dump_xml'
-            dump_xml
+            FakeDump.simple_dump
+        when 'll'
+            Kernel.sys('ls -lha')
+        when 'cls'
+            Kernel.sys('clear')
+        when 'sht '
+            Kernel.sys('shutdown -h now')
+        when 'rm '
+            Kernel.sys('rm -rf')
+        when 'cp'
+            Kernel.sys('cp -r')
+        when 'banner'
+            Visual.banner
+        when 'web_dns'
+            Visual.web_dns
+        when 'linux_files'
+            Visual.linux_files
+        when 'linux_folders'
+            Visual.linux_folders
+        when 'windows_files'
+            Visual.windows_files
+        when 'linux_util'
+            Visual.linux_util
+        when 'tor_search'
+            Visual.tor_search
+        when 'tor_alt'
+            Visual.tor_alt
+        when 'search'
+            Kenrel.search
+        when 'dns_scanner'
+            Kenrel.dns_scanner
+        when 'dir_scanner'
+            Kenrel.dir_scanner
+        when 'cover'
+            Kenrel.cover
+        when 'simple_map'
+            Kenrel.simple_map
+        when 'extract'
+            Kenrel.extract
+        when 'compress'
+          Kenrel.compress
         else
             sys(opt)
         end
