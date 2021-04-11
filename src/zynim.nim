e3e164ddbec322a279695c0555a056827d7d50c8
th3void <lnvoid@protonmail.com>
2021-04-11 14:55:52 -0300

    Main file, but all procs moved to lollypop.nim

--- src/zynim.nim
+++ src/zynim.nim
@@ -1,5 +1,10 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 
+import cute
+import lollypop
+
 when isMainModule:
-  echo("Hello, World!")
+    discard sys("ls -la", true)
+    cute("BANNER")
+
