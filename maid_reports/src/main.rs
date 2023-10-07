af63fbe6572690ee572bdea474de99a20919aae1
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-07 16:51:22 -0300

    Update Race Condition in tokio

--- maid_reports/src/main.rs
+++ maid_reports/src/main.rs
@@ -1,5 +1,5 @@
-mod meow;
 mod api;
+mod meow;
 
 use crate::api::api::*;
 use warp::Filter;
@@ -7,16 +7,31 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // Define a filter that matches a GET request to the /jsonl endpoint.
-    let jsonl_route = warp::path!("jsonl")
-        .and(warp::get())
-        .map(|| {
-            // Read the JSONL file and return it as a response.
-            let file_content = read_jsonl_file();
-            warp::reply::with_header(file_content, warp::http::header::CONTENT_TYPE, "application/json")
-        });
+     // Replace "your_jsonl_file.jsonl" with the path to your JSONL file.
+    let config = read_meow("/var/maid/maid_lists/embedded/config.meow", false);
+    let file_path = format!("{}{}", config["REPORT_BASE_PATH"], config["REPORT_LOG"]);
+ 
+    let jsonl_route = warp::path!("jsonl").and(warp::get()).map(|| {
+        // Read the JSONL file and return it as a response.
+        let file_content = read_file(file_path);
+        warp::reply::with_header(
+            file_content,
+            warp::http::header::CONTENT_TYPE,
+            "application/json",
+        )
+    });
+
+
+    let index_page = config["INDEX"].to_string();
+    let index = warp::path!("/").and(warp::get()).map(|| {
+        let file_content = read_file(index_page);
+        warp::reply::with_header(
+            file_content,
+            warp::http::header::CONTENT_TYPE,
+            "text/html",
+        )
+    });
 
     // Start the Warp server.
-    warp::serve(jsonl_route)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    warp::serve(jsonl_route, index).run(([127, 0, 0, 1], 8000)).await;
 }
