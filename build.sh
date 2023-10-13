5aacd57eb3bd3983294e3758c2f8f36ddf7f4716
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-12 22:28:45 -0300

    Added rustup

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
-echo " ---- BUILD AND INSTALL ---- "
 
+echo " ----  INSTALLING RUST  ---- "
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo " ----     GIT CLONE     ---- "
+git clone --depth 1 https://github.com/th3Maid/MaidRunner.git --single-branch --branch master
+
+echo " ---- BUILD AND INSTALL ---- "
 sudo su
 mkdir /var/maid
 cp maid_lists/ /var/maid -R
