c3de1f4921aec6c4ab0999a9a0b865b9b16b33af
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-18 05:45:57 -0300

    Removed nala package from build

--- build.sh
+++ build.sh
@@ -9,12 +9,11 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo
 echo "INSTALL DEPENDENCIES"
-sudo apt install nala -y 
 sudo mkdir /var/maid
 sudo cp maid_lists/ /var/maid -R
 sudo chown $(whoami):$(whoami) /var/maid -R
-sudo nala update
-sudo nala install -y nmap curl xxd libc6 exiftool traceroute wget iproute2 whois dnsutils dirb dnsenum tree htop tcpdump openssl iftop
+sudo apt update
+sudo apt install -y nmap curl xxd libc6 exiftool traceroute wget iproute2 whois dnsutils dirb dnsenum tree htop tcpdump openssl iftop
 
 echo
 echo "BUILD AND INSTALL"
