169e791e7977d055c5ed14066e8be34c116ebdbf
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-19 22:18:15 -0300

    Added charts.js and charts section

---
+++ maid_ui/src/assets/js/charts.js
@@ -0,0 +1,27 @@
+(async function () {
+    const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+    ];
+
+    new Chart(
+        document.getElementById('acquisitions'),
+        {
+            type: 'bar',
+            data: {
+                labels: data.map(row => row.year),
+                datasets: [
+                    {
+                        label: 'Acquisitions by year',
+                        data: data.map(row => row.count)
+                    }
+                ]
+            }
+        }
+    );
+})();
