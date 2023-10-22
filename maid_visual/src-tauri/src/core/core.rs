cd5806fc1786555bb7fe2462731ff58539be60cb
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-22 00:53:47 -0300

    Implement value return to front end with success

---
+++ maid_visual/src-tauri/src/core/core.rs
@@ -0,0 +1,53 @@
+extern crate rusqlite;
+use rusqlite::Error;
+use rusqlite::Connection;
+
+
+
+pub fn select_report_from_db(conn: &Connection, from: String) -> Result<Vec<String>, Error> {
+    let mut result = conn.prepare(
+        &format!("SELECT * FROM process_results WHERE source_from=\"{}\"; ", from)
+    ).expect("Query Failed");
+
+    let mut formated_rows: Vec<String> = Vec::new();
+
+    let rows = result.query_map([], |row| {
+        // Create a ProcessResult struct and populate its fields
+        Ok(
+            format!(
+                "{{ 'id' : {}, 'session' : {}, 'session_description' : {}, 'source_from' : {}, 'source_command' : {}, 'source_detail' : {}, 'source_description' : {}, 'timestemp' : {}, 'returned_status' : {}, 'formated_stdout' : {}, 'formated_stderr' : {}, 'debug' : {} }}
+                ",
+                row.get::<usize, i32>(0).expect("fodase"),
+                row.get::<usize, String>(1).expect("fodase"),
+                row.get::<usize, String>(2).expect("fodase"),
+                row.get::<usize, String>(3).expect("fodase"),
+                row.get::<usize, String>(4).expect("fodase"),
+                row.get::<usize, String>(5).expect("fodase"),
+                row.get::<usize, String>(6).expect("fodase"),
+                row.get::<usize, String>(7).expect("fodase"),
+                row.get::<usize, String>(8).expect("fodase"),
+                row.get::<usize, String>(9).expect("fodase"),
+                row.get::<usize, String>(10).expect("fodase"),
+                row.get::<usize, i32>(11).expect("fodase"),
+            )
+        )
+    })?;
+
+
+    for row in rows {
+        formated_rows.push(row?);
+    }
+
+    Ok(formated_rows)
+}
+
+#[tauri::command]
+pub fn select_report(from: String) -> Vec<String> {
+    let connection = Connection::open("/var/maid/maid_lists/report/archive.db").expect("fail");
+
+    match select_report_from_db(&connection, from) {
+        Ok(process_results) => process_results,
+        Err(_) => vec![],
+    } 
+    
+}
---
+++ maid_visual/src-tauri/src/core/structs.rs
@@ -0,0 +1,43 @@
+pub struct ProcessResult<
+    'session,
+    'session_description,
+    'source_from,
+    'source_command,
+    'source_detail,
+    'source_description,
+    'timestemp,
+    'returned_status,
+> {
+    pub id: i32,
+    pub session: &'session str,
+    pub session_description: &'session_description str,
+    pub source_from: &'source_from str,
+    pub source_command: &'source_command str,
+    pub source_detail: &'source_detail str,
+    pub source_description: &'source_description str,
+    pub timestemp: &'timestemp str,
+    pub returned_status: &'returned_status str,
+    pub formated_stdout: Vec<String>,
+    pub formated_stderr: Vec<String>,
+    pub debug: i32,
+}
+
+impl ProcessResult<'_, '_, '_, '_, '_, '_, '_, '_> {
+    // Function to serialize the struct to a string
+    pub fn to_string(&self) -> String {
+        format!(
+            "id: {}, session: {}, session_description: {}, source_from: {}, source_command: {}, \
+            source_detail: {}, source_description: {}, timestemp: {}, returned_status: {}, debug: {}",
+            self.id,
+            self.session,
+            self.session_description,
+            self.source_from,
+            self.source_command,
+            self.source_detail,
+            self.source_description,
+            self.timestemp,
+            self.returned_status,
+            self.debug
+        )
+    }
+}
