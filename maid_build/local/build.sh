8cff3238e116d84179c5dcd986e72b71b3c77a52
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-24 02:02:14 -0300

    Add script for development builds

---
+++ maid_build/local/build.sh
@@ -0,0 +1,31 @@
+echo "MR Build System"
+
+echo && echo "Install dependencies"
+sudo apt install aptitude
+sudo aptitude update -y
+sudo aptitude install -y nmap curl xxd libc6 exiftool traceroute wget iproute2 whois dnsutils dirb dnsenum tree htop iftop clang 
+sudo aptitude install -y libwebkit2gtk-4.0-dev build-essential curl wget file libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev
+
+echo && echo "Move config folders to /var"
+sudo mkdir /var/maid
+sudo cp maid_lists/ /var/maid -R
+sudo chown $(whoami):$(whoami) /var/maid -R
+
+echo && echo "SNAP Setup"
+sudo aptitude install snapd -y
+sudo snap install vlc -y
+
+echo && echo "Install virtualization"
+sudo aptitude install virt-manager docker -y
+
+echo && echo "Cargo build monorepo"
+cargo build --release --manifest-path maid_visual/src-tauri/Cargo.toml
+cargo build --release --manifest-path maid_runner/Cargo.toml
+cargo build --release --manifest-path maid_api/Cargo.toml
+
+echo && echo "Move applications to app/"
+mkdir /home/$(whoami)/app
+cp ./maid_visual/src-tauri/target/release/maid_visual  /home/$(whoami)/app
+cp ./maid_runner/target/release/maid_runner /home/$(whoami)/app
+cp ./maid_api/target/release/maid_api /home/$(whoami)/app
+
