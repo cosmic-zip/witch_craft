65c34b066e9599a49cd865dade2b9b3bbd088219
th3maid <36008397+th3maid@users.noreply.github.com>
2023-10-25 03:30:07 -0300

    Fix core returning empty, now it return "none", debug returns true or false strings instead numbers

--- maid_visual/src-tauri/src/core/core.rs
+++ maid_visual/src-tauri/src/core/core.rs
@@ -1,11 +1,26 @@
 extern crate rusqlite;
 use rusqlite::Error;
 use rusqlite::Connection;
-use serde_json::json;
 
 
+pub fn is_string_mem_empty(mem_loc: String) -> String {
+    if mem_loc.len() == 0 || mem_loc == "[]".to_string() {
+        "none".to_string()
+    } else {
+        mem_loc
+    }
+}
 
-pub fn  select_report_from_db(conn: &Connection, from: String, size: i32) -> Result<Vec<Vec<String>>, Error> {
+pub fn is_bool_mem_empty(mem_bool: i32) -> String {
+    if mem_bool == 0 {
+        "false".to_string()
+    } else {
+        "true".to_string()
+    }
+} 
+
+
+pub fn select_report_from_db(conn: &Connection, from: String, size: i32) -> Result<Vec<Vec<String>>, Error> {
     let mut result;
 
     if from == "all" {
@@ -24,18 +39,18 @@ pub fn  select_report_from_db(conn: &Connection, from: String, size: i32) -> Res
         // Create a ProcessResult struct and populate its fields
         Ok(
             vec![
-                format!("{}", row.get::<usize, i32>(0).expect("fodase")),
-                row.get::<usize, String>(1).expect("fodase"),
-                row.get::<usize, String>(2).expect("fodase"),
-                row.get::<usize, String>(3).expect("fodase"),
-                row.get::<usize, String>(4).expect("fodase"),
-                row.get::<usize, String>(5).expect("fodase"),
-                row.get::<usize, String>(6).expect("fodase"),
-                row.get::<usize, String>(7).expect("fodase"),
-                row.get::<usize, String>(8).expect("fodase"),
-                row.get::<usize, String>(9).expect("fodase"),
-                row.get::<usize, String>(10).expect("fodase"),
-                format!("{}", row.get::<usize, i32>(11).expect("fodase")),
+                format!("{}", row.get::<usize, i32>(0).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(1).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(2).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(3).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(4).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(5).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(6).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(7).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(8).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(9).expect("Error at src.core.selec_report_db")),
+                is_string_mem_empty(row.get::<usize, String>(10).expect("Error at src.core.selec_report_db")),
+                is_bool_mem_empty(row.get::<usize, i32>(11).expect("Error at src.core.selec_report_db")),
             ]
         )
     })?;
