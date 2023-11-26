cc2f2afecf3396011d0bb18764f1cb047bcde2f5
th3maid <36008397+th3maid@users.noreply.github.com>
2023-11-26 16:57:48 -0300

    fully implement firewall module

--- maid_runner/src/modules/firewall/firewall.rs
+++ maid_runner/src/modules/firewall/firewall.rs
@@ -1,20 +1,37 @@
 use crate::core::messages::standard_messages;
 use crate::core::structs::ProcessInit;
 use crate::core::utils::*;
-use crate::modules::firewall::firewall_structs::*;
+use crate::modules::firewall::firewall_structs::SimpleRule;
 
-pub fn firewall(option: &str, debug: bool) -> bool {
+pub fn firewall_preset(option: &str, debug: bool) -> bool {
+
+
+    let mut rules = Vec::new();
+    let mut result:bool = false;
 
     match option {
 
         "kill" => {
-            let instance = ProcessInit {
-                source: &"sudo iptables -F && sudo iptables -P INPUT DROP && sudo iptables -P FORWARD DROP && sudo iptables -P OUTPUT DROP",
-                source_from: "firewall_kill",
-                source_description: "Set firewall rules",
-                debug: debug,
-            };
-            system_command_exec(instance)
+            rules = vec![
+                "iptables -F",
+                "iptables -P INPUT DROP",
+                "iptables -P FORWARD DROP",
+                "iptables -P OUTPUT DROP",
+            ];
+        }
+
+        "hardned" => {
+
+            rules = vec![
+                "iptables -F",
+                "iptables -P INPUT DROP",
+                "iptables -P FORWARD DROP",
+                "iptables -P OUTPUT DROP",
+                "iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT",
+                "iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT",
+                "iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT",
+            ];
+
         }
 
         _ => {
@@ -29,17 +46,66 @@ pub fn firewall(option: &str, debug: bool) -> bool {
 
     }
 
+    for rule in rules {
+
+        let instance = ProcessInit {
+            source: rule,
+            source_from: "firewall_kill",
+            source_description: "Set firewall rules",
+            debug: debug,
+        };
+        result = system_command_exec(instance);
+    }
+
+    return result;    
+
 }
 
-pub fn shell_lookup(system_input: Vec<String>) -> bool {
+pub fn firewall(ruleset: SimpleRule, debug: bool) -> bool {
+
+    let rule = format!(
+        "iptable -A {} -p {} --dport {} -j {}",
+        ruleset.chain,
+        ruleset.protocol,
+        ruleset.destination_port,
+        ruleset.table
+    );
+
+    let instance = ProcessInit {
+        source: &rule,
+        source_from: "firewall_kill",
+        source_description: "Set firewall rules",
+        debug: debug,
+    };
+    
+    system_command_exec(instance)
+
+}
+
+
+
+pub fn shell_firewall(system_input: Vec<String>) -> bool {
     let cmd_arg_name = system_input[2].as_str();
 
     match cmd_arg_name {
-        "--firewall" => {
+        "--firewall-preset" => {
             let debug = gsv_debug(gsv(system_input.clone(), "--debug"));
             let option = &gsv(system_input.clone(), "--option");
-            
-            firewall(option, debug)         
+            firewall_preset(option, debug)         
+        }
+
+        "--firewall" => {
+
+            let debug = gsv_debug(gsv(system_input.clone(), "--debug"));
+            let command = SimpleRule {
+                table: &gsv(system_input.clone(), "--table"),
+                chain: &gsv(system_input.clone(), "--chain"),
+                protocol: &gsv(system_input.clone(), "--protocol"),
+                destination_port: &gsv(system_input.clone(), "--port"),
+            };
+
+            firewall(command, debug)
+
         }
 
         _ => {
