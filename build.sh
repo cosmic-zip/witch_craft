ceb77adf0396f32b159878cf8395491996ea3506
th3maid <36008397+th3maid@users.noreply.github.com>
2023-11-28 11:55:16 -0300

    fix 7x script

--- build.sh
+++ build.sh
@@ -13,8 +13,7 @@ sudo chown $(whoami):$(whoami) /var/maid -R
 
 echo && echo  "Uncompress files"
 7z x /var/maid/maid_lists/malware/full.csv.7z.001
-mv /var/maid/maid_lists/malware/full.csv /var/maid/maid_lists/malware/malware_hash.bin
-mv /var/maid/maid_lists/malware/malware_hash.bin /var/maid/maid_lists/general/
+mv ./full.csv /var/maid/maid_lists/general/malware_hash.bin
 rm full.csv
 
 echo && echo "SNAP Setup"
