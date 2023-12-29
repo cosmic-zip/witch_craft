535475f4285f61cc7727ce4ee1e612adf6249226
th3maid <36008397+th3maid@users.noreply.github.com>
2023-12-29 14:30:30 -0300

    add osint domains hosts names (dns) names update script

---
+++ maid_build/artifacts/hosts_list_downloader.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/social-only/hosts -O social
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-only/hosts -O porn
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-only/hosts -O gambling
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-only/hosts -O fakenews
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts -O base
