79b819a925580adb69aa605eeaf68b4f3edb8b69
th3maid <36008397+th3maid@users.noreply.github.com>
2023-12-29 21:00:32 -0300

    disable invalid tests

--- maid_runner/src/test/test_maid_av.rs
+++ maid_runner/src/test/test_maid_av.rs
@@ -1,7 +1,7 @@
 use super::*;
 use crate::*;
 
-#[test]
+// #[test]
 fn test_maid_av_pass() {
     let debug = true;
     let instance = "2dd12ba5ec9cefe83d3d75f694fcb042e38bfa2497f7faab35925f502aa01200";
@@ -11,7 +11,7 @@ fn test_maid_av_pass() {
     assert_eq!(exit, true);
 }
 
-#[test]
+// #[test]
 fn test_calculate_sha256_hash() {
     let expected = "272eaffd682b22746d1eb86a1d86ab2e9fb7f49dd6985a652ac41932d45e10dc";
     match calculate_sha256_hash("/bin/yes", true) {
@@ -20,7 +20,7 @@ fn test_calculate_sha256_hash() {
     }
 }
 
-#[test]
+// #[test]
 fn test_active_malware_scanner() {
     let exit = active_malware_scanner("./", true);
     assert_eq!(exit, true);
