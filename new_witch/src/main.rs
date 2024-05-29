c671c228b229332839accb4e3d54faf0c3b100ed
prism-zip <36008397+prism-zip@users.noreply.github.com>
2024-05-29 13:52:34 -0300

    rustfmt

--- new_witch/src/main.rs
+++ new_witch/src/main.rs
@@ -1,3 +1,10 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+mod modules;
+
+use crate::modules::core::core::*;
+
 fn main() {
     println!("Hello, world!");
 }
--- new_witch/src/test/test_start.rs
+++ new_witch/src/test/test_start.rs
@@ -1,11 +1,37 @@
+#[allow(unused_imports)]
 use super::*;
 use crate::*;
 
 #[test]
 fn test_start() {
-    
     println!("TEST HERE!");
 
     let exit = true;
     assert_eq!(exit, true);
+}
+
+#[test]
+fn test_string_to_command() {
+    let argsv = vec![
+        "some.rs",
+        "--some",
+        "val",
+        "--another",
+        "ant",
+        "--numeric",
+        "123",
+        "--path",
+        "/etc/hosts",
+    ];
+
+    let expected = "command -S val -A ant -N 123 -P /etc/hosts";
+    let mut newvec = Vec::<String>::new();
+    for x in argsv {
+        newvec.push(x.to_string());
+    }
+
+    let meta_string = "command -S @@@some -A @@@another -N @@@numeric -P @@@path";
+    let out = lazy_loop(meta_string, newvec);
+
+    assert_eq!(expected, out);
 }
