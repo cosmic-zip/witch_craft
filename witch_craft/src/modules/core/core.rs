96800bd70b3e9c923d91d58d642afd611df42f77
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-08-08 07:49:22 -0300

    Update functions documentation, run and code cleanup

--- witch_craft/src/modules/core/core.rs
+++ witch_craft/src/modules/core/core.rs
@@ -75,48 +75,6 @@ pub fn search_key(key: String, vector: Vec<String>) -> String {
     return "none".to_string();
 }
 
-pub fn lazy_loop(meta_string: &str, argsv: Vec<String>) -> String {
-    let meta: Vec<&str> = meta_string.split_whitespace().collect();
-    let mut cmds: String = meta_string.to_string();
-
-    for item in meta {
-        if item.contains("http") {
-            // Fix for urls like https://@@domain/some/thing
-            let aaaa = item.split("/");
-            let mut new = String::new();
-
-            for c in aaaa {
-                if c.contains(TONK) {
-                    let opt = c.replace(TONK, "");
-                    let val = search_value(opt, argsv.clone());
-                    new = item.replace(c, &val);
-                }
-            }
-
-            cmds = cmds.replace(item, &new);
-        }
-
-        if item.contains(TONK) & !item.contains("http") {
-            let opt = item.replace(TONK, "");
-            let val = search_value(opt, argsv.clone());
-            cmds = cmds.replace(item, &val);
-        }
-    }
-
-    return cmds;
-}
-
-pub fn raw_exec(command_line: String) -> Option<Output> {
-    let mut parts = command_line.split_whitespace();
-    let command = parts.next().expect("No command found");
-    let args: Vec<&str> = parts.collect();
-
-    match Command::new(command).args(&args).output() {
-        Ok(output) => Some(output),
-        Err(_) => None,
-    }
-}
-
 fn split_line(input: &str, max_length: usize) -> Vec<String> {
     let mut lines = Vec::new();
     let mut start = 0;
@@ -128,40 +86,15 @@ fn split_line(input: &str, max_length: usize) -> Vec<String> {
     lines
 }
 
-pub fn lazy_exec(command_line: String, pretty: bool) -> i32 {
-    match raw_exec(command_line) {
-        Some(output) => {
-            if output.status.success() {
-                let stdout = String::from_utf8_lossy(&output.stdout);
-                let lines = stdout.split("\n");
-                for line in lines {
-                    let result = split_line(&line, 180);
-                    for line in result {
-                        println!("\t{}", line);
-                    }
-                }
-            } else {
-                let stderr = String::from_utf8_lossy(&output.stderr);
-                eprintln!("\n{}\n", stderr);
-            }
-            println!("");
-            output.status.code().unwrap_or(-1)
-        }
-        None => return 0,
-    }
-}
-
-pub fn bob(set: DataSet, argsv: Vec<String>) -> i32 {
-    raise(&set.name, 0);
-    let cmd = lazy_loop(&set.meta, argsv);
-    let out = lazy_exec(cmd, false);
-    if out != 0 {
-        return out;
-    }
-
-    return 0;
-}
-
+/// Generates a pretty-formatted manual from `db.json`.
+///
+/// Parses command strings and adds fancy shell images in Chafa format.
+/// Prints the formatted manual result.
+///
+/// # Functionality
+/// - Parses command strings stored in `db.json`.
+/// - Integrates Chafa images for visual enhancements.
+/// - Outputs the result with improved formatting.
 pub fn magic_docs() {
     let data: Vec<DataSet> = data();
 
@@ -194,4 +127,150 @@ pub fn magic_docs() {
 
     }
 
+}
+
+/// Parses a UwU formatted command string.
+///
+/// Replaces placeholders `@@bar` in `meta_string` with values from `argsv`.
+///
+/// # Parameters
+/// - `meta_string`: The command string to be parsed.
+/// - `argsv`: A vector of strings where each pair represents a `--for` key and its value.
+///
+/// # Exemple:
+/// - cmd: foo --flag @@bar
+/// - input: foo --bar "some value"
+/// - out: foo --flag "some value"
+pub fn lazy_loop(meta_string: &str, argsv: Vec<String>) -> String {
+    let meta: Vec<&str> = meta_string.split_whitespace().collect();
+    let mut cmds: String = meta_string.to_string();
+
+    for item in meta {
+        if item.contains("http") {
+            let aaaa = item.split("/");
+            let mut new = String::new();
+
+            for c in aaaa {
+                if c.contains(TONK) {
+                    let opt = c.replace(TONK, "");
+                    let val = search_value(opt, argsv.clone());
+                    new = item.replace(c, &val);
+                }
+            }
+
+            cmds = cmds.replace(item, &new);
+        }
+
+        if item.contains(TONK) & !item.contains("http") {
+            let opt = item.replace(TONK, "");
+            let val = search_value(opt, argsv.clone());
+            cmds = cmds.replace(item, &val);
+        }
+    }
+
+    return cmds;
+}
+
+/// Executes a command string on the host system.
+///
+/// Takes a command string that has been parsed by `lazy_loop` or is a static command string.
+/// Executes the command using the system's command interpreter and returns the output.
+///
+/// # Parameters
+/// - `command_line`: The command string to execute.
+///
+/// # Returns
+/// - `Option<Output>`: The output of the executed command. Returns `None` if the command execution fails.
+pub fn raw_exec(command_line: String) -> Option<Output> {
+    let mut parts = command_line.split_whitespace();
+    let command = parts.next().expect("No command found");
+    let args: Vec<&str> = parts.collect();
+
+    match Command::new(command).args(&args).output() {
+        Ok(output) => Some(output),
+        Err(_) => None,
+    }
+}
+
+/// A wrapper for `raw_exec` that handles command execution and output formatting.
+///
+/// Executes `raw_exec` with the given command string. If `pretty` is `true`, it formats the output.
+/// Prints the command's standard output on success and the standard error if any errors occur.
+/// Returns the exit status code of the command as an `i32`.
+///
+/// # Parameters
+/// - `command_line`: The command string to execute.
+/// - `pretty`: If `true`, formats the command output for better readability.
+///
+/// # Returns
+/// - `i32`: The exit status code of the executed command, which is zero or greater if successful.
+pub fn lazy_exec(command_line: String, pretty: bool) -> i32 {
+    match raw_exec(command_line) {
+        Some(output) => {
+            if output.status.success() {
+                let stdout = String::from_utf8_lossy(&output.stdout);
+                let lines = stdout.split("\n");
+                for line in lines {
+                    let result = split_line(&line, 180);
+                    for line in result {
+                        println!("\t{}", line);
+                    }
+                }
+            } else {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                eprintln!("\n{}\n", stderr);
+            }
+            println!("");
+            output.status.code().unwrap_or(-1)
+        }
+        None => return 0,
+    }
+}
+
+/// Executes the specified command (`cmd`) `count` times.
+///
+/// For example, `foo --count 10 --foo bar` runs `--foo bar` 10 times.
+/// Useful for automation and stress testing.
+///
+/// # Notes
+/// - Commands are executed immediately in interactive mode; no scheduling support.
+///
+/// # Arguments
+/// - `cmd`: The command to execute.
+/// - `count`: Number of executions.
+///
+/// # Example
+/// ```
+/// foo --count 5 --bar example
+/// ```
+pub fn lazy_exec_loop(argsv: Vec<String>, cmd: &str) -> i32 {
+    let out = search_value("count".to_string(), argsv.clone());
+    let range: i32 = out.parse().unwrap_or(1);
+    let mut exit = 0;
+    for i in 1..range {
+        let exec = DataSet::from_str("name", "some.thing", cmd);
+        exit = bob(exec, argsv.clone());
+    }
+    return exit;
+}
+
+/// Calls `lazy_exec` and `lazy_loop` with the provided arguments.
+///
+/// This function uses `DataSet` and `argsv` (a `Vec<String>` of terminal arguments) to:
+/// - Parse and execute the command string found in `set.meta`.
+///
+/// # Arguments
+/// - `set`: Contains the command metadata.
+/// - `argsv`: Vector of terminal arguments to be parsed.
+///
+/// # Example
+/// ```
+/// let dataset = DataSet { /* ... */ };
+/// let args = vec!["--flag".to_string(), "value".to_string()];
+/// bob(dataset, args);
+/// ```
+pub fn bob(set: DataSet, argsv: Vec<String>) -> i32 {
+    raise(&set.name, 0);
+    let cmd = lazy_loop(&set.meta, argsv);
+    return lazy_exec(cmd, false);
 }
