72db97331db033c4531248ffb48edcf9e0a481d3
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-08-12 22:41:38 -0300

    add base network

--- witch_craft/src/modules/network/structs.rs
+++ witch_craft/src/modules/network/structs.rs
@@ -1,1 +1,42 @@
+use reqwest::blocking::Client;
+use std::collections::HashMap;
 
+struct Response {
+    url: String,
+    status: String,
+    body: String,
+}
+
+struct Request {
+    url: String,
+    method: String,
+    body: Option<HashMap<String, String>>,
+}
+
+impl Request {
+    fn new() -> Self {
+        Request {
+            url: String::new(),
+            method: String::new(),
+            body: None,
+        }
+    }
+
+    fn make(&self) -> Response {
+        let client = Client::new();
+        let res = match self.method.as_str() {
+            "POST" => client.post(&self.url).json(&self.body).send().unwrap(),
+            "GET" => client.get(&self.url).send().unwrap(),
+            _ => panic!("Unsupported method"),
+        };
+
+        let status = res.status().to_string();
+        let body = res.text().unwrap();
+
+        Response {
+            url: self.url.clone(),
+            status,
+            body,
+        }
+    }
+}
