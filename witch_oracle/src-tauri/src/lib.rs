d2028627a1c8aa0f8fe46917f97b333b2b40bdcb
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-10-19 08:06:15 -0300

    Added read log to json function

--- witch_oracle/src-tauri/src/lib.rs
+++ witch_oracle/src-tauri/src/lib.rs
@@ -1,8 +1,4 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
