cda581d6d1cfb9e8e042eca502edef7d6ca20868
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-10-25 21:26:03 -0300

    Added env export WITCH_SPELLS_ROOT_DIR

--- build.sh
+++ build.sh
@@ -36,6 +36,8 @@ sudo mkdir -p /var/witch_craft
 sudo cp -r witch_spells/ /var/witch_craft
 sudo chown -R $(whoami):$(whoami) /var/witch_craft
 7z x /var/witch_craft/witch_spells/osint/archive.7z.001
+echo 'export WITCH_SPELLS_ROOT_DIR=/var/witch_craft/witch_spells/' >> ~/.bash_profile
+export WITCH_SPELLS_ROOT_DIR=/var/witch_craft/witch_spells/
 
 # Wordlist
 echo && echo "Wordlists are big (16GB) so, if you want or need then, just run:"
