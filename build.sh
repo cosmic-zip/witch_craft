ec73a580da0e2ede0e281c3eb17c74aa13e4897d
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-10-29 23:30:04 -0300

    Update build

--- build.sh
+++ build.sh
@@ -2,33 +2,11 @@
 set -e
 
 echo && echo "Install dependencies"
-#!/bin/bash
-
-# Check if the package manager is apt or yum
-if command -v apt &> /dev/null; then
-    package_manager="apt"
-elif command -v yum &> /dev/null; then
-    package_manager="yum"
-else
-    echo "Unsupported package manager. Please install the packages manually."
-    exit 1
-fi
-
-# Install packages
-if [ "$package_manager" == "apt" ]; then
-    echo && echo "Trying to install chromium, its used for social media OSINT!"
-    sudo snap install chromium
-
-    echo && echo "Install apt depedencies"
-    sudo apt update
-    sudo apt install -y nmap whois dirb dnsenum libc-bin iproute2 xxd iptables coreutils wget curl \
-    dnsutils traceroute openssl openssh-server xattr libimage-exiftool-perl tor foremost pkg-config \
-    libssl-dev steghide doas nala libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev 7zip \
-    libayatana-appindicator3-dev librsvg2-dev
-elif [ "$package_manager" == "yum" ]; then
-    sudo yum update -y
-    sudo yum install -y nmap dirb dnsenum glibc-utils xxd iptables-utils iproute wget curl bind-utils traceroute
-fi
+sudo apt update
+sudo apt install -y nmap whois dirb dnsenum libc-bin iproute2 xxd iptables coreutils wget curl \
+dnsutils traceroute openssl openssh-server xattr libimage-exiftool-perl tor foremost pkg-config \
+libssl-dev steghide doas nala libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev 7zip \
+libayatana-appindicator3-dev librsvg2-dev
 
 # Install data
 echo && echo "Install witch_spells data"
@@ -48,13 +26,3 @@ echo && echo "Cargo build"
 cargo build --release --manifest-path witch_craft/Cargo.toml
 chmod +x ./witch_craft/target/release/witch_craft
 sudo cp -r ./witch_craft/target/release/witch_craft /bin
-
-
-# Test and print status for each binary
-witch_craft
-if [ $? -eq 0 ]; then
-    echo "Exit code is 0, all good!"
-else
-    echo "Exit code is not 0, something went wrong."
-    exit
-fi
