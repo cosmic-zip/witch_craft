a53735bd4776489f9cc8577eb2d1d6d524ef4db2
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-11-07 13:39:40 -0300

    repo cleanup

--- witch_oracle/src-tauri/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    witch_oracle_lib::run()
-}
-
-#[cfg(test)]
-mod test;
--- witch_oracle/src/scripts/ui.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $("#main_apps_menu").click(function () {
-    $("#sidebar").toggle();
-  });
-});
