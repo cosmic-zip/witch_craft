de207b32a590d04ba76736506d6620475f9e33ec
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-11-07 15:42:27 -0300

    Added rootkit bind

--- witch_craft/src/modules/blackcat/blackcat.rs
+++ witch_craft/src/modules/blackcat/blackcat.rs
@@ -1,5 +1,11 @@
-use crate::{core::types::Closure, modules::blackcat::backend::blackcat_av};
+use crate::{
+    core::types::Closure,
+    modules::blackcat::backend::{blackcat_av, blackcat_chkrootkit_bind},
+};
 
 pub fn api() -> Closure {
-    vec![("blackcat.av", blackcat_av)]
+    vec![
+        ("blackcat.av", blackcat_av),
+        ("blackcat.rootkit", blackcat_chkrootkit_bind),
+    ]
 }
