fcdaab3521a6cfa3a224829260151fb3c35e3f57
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-11-13 18:22:57 -0300

    Update build

--- build.sh
+++ build.sh
@@ -7,14 +7,14 @@ sudo apt update
 sudo apt install -y nmap whois dirb dnsenum libc-bin iproute2 xxd iptables coreutils wget curl \
 dnsutils traceroute openssl openssh-server xattr libimage-exiftool-perl tor foremost pkg-config \
 libssl-dev steghide libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev 7zip \
-libayatana-appindicator3-dev librsvg2-dev chromium
+libayatana-appindicator3-dev librsvg2-dev chromium-browser
 
 # Install data
 echo && echo "Install witch_spells data"
 sudo cp -r witch_spells/ /var/
 sudo chown -R $(whoami):$(whoami) /var/witch_spells
-7z x /var/witch_spells/archive/osint/Archive.7z.001 -o/var/witch_spells/archive/osint/
-7z x /var/witch_spells/archive/malware/full.7z.001 -o/var/witch_spells/archive/malware/
+7z x /var/witch_spells/archive/osint/Archive.7z.001 -o/var/witch_spells/archive/osint/"
+7z x /var/witch_spells/archive/malware/full.7z.001 -o/var/witch_spells/archive/malware/"
 echo 'export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive/' >> ~/.bash_profile
 export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive/
 
