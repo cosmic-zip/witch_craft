5001f2eca9245ae416c29e438c982ccbb73bfc2a
cosmic-zip <36008397+cosmic-zip@users.noreply.github.com>
2024-11-13 22:52:36 -0300

    Update build

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ fi
 echo && echo "Install witch_spells data"
 sudo cp -r witch_spells/ /var/
 sudo chown -R $(whoami):$(whoami) /var/witch_spells
-7z x /var/witch_spells/archive/osint/Archive.7z.001 -o/var/witch_spells/archive/osint/"
-7z x /var/witch_spells/archive/malware/full.7z.001 -o/var/witch_spells/archive/malware/"
+7z x /var/witch_spells/archive/osint/Archive.7z.001 -o/var/witch_spells/archive/osint/
+7z x /var/witch_spells/archive/malware/full.7z.001 -o/var/witch_spells/archive/malware/
 echo 'export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive/' >> ~/.bash_profile
 export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive/
 
@@ -34,4 +34,3 @@ cargo build --release --manifest-path witch_craft/Cargo.toml
 chmod +x ./witch_craft/target/release/witch_craft
 sudo cp -r ./witch_craft/target/release/witch_craft /bin/witchcraft
 sudo ln -s /bin/witchcraft /bin/witchy
-ç
