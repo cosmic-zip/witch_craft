438b4f221c3d175606ffeeb3c47c7f28f2fbc994
Cosmic <36008397+cosmic-zip@users.noreply.github.com>
2024-11-13 23:36:34 -0300

    Feature/improve blackcat av (#91)
    Revision 0.20.56

--- build.sh
+++ build.sh
@@ -7,15 +7,21 @@ sudo apt update
 sudo apt install -y nmap whois dirb dnsenum libc-bin iproute2 xxd iptables coreutils wget curl \
 dnsutils traceroute openssl openssh-server xattr libimage-exiftool-perl tor foremost pkg-config \
 libssl-dev steghide libwebkit2gtk-4.1-dev build-essential curl wget file libxdo-dev 7zip \
-libayatana-appindicator3-dev librsvg2-dev chromium
+libayatana-appindicator3-dev librsvg2-dev chromium-browser
+
+# Install rust if not exists
+if ! command -v rustc &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
 
 # Install data
 echo && echo "Install witch_spells data"
 sudo cp -r witch_spells/ /var/
 sudo chown -R $(whoami):$(whoami) /var/witch_spells
-echo "Run: 7z x /var/witch_spells/archive/osint/Archive.7z.001 -o/var/witch_spells/archive/osint/"
-echo 'export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive' >> ~/.bash_profile
-export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive
+7z x /var/witch_spells/archive/osint/Archive.7z.001 -o/var/witch_spells/archive/osint/
+7z x /var/witch_spells/archive/malware/full.7z.001 -o/var/witch_spells/archive/malware/
+echo 'export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive/' >> ~/.bash_profile
+export WITCH_SPELLS_ROOT_DIR=/var/witch_spells/archive/
 
 # Wordlist
 echo && echo "Wordlists are big (16GB) so, if you want or need then, just run:"
@@ -26,4 +32,5 @@ echo "7z x /var/witch_craft/witch_spells/wordlists/moth.pwned.7z.001"
 echo && echo "Cargo build"
 cargo build --release --manifest-path witch_craft/Cargo.toml
 chmod +x ./witch_craft/target/release/witch_craft
-sudo cp -r ./witch_craft/target/release/witch_craft /bin
+sudo cp -r ./witch_craft/target/release/witch_craft /bin/witchcraft
+sudo ln -s /bin/witchcraft /bin/witchy
